7bea2b5db51ed07f320d747fa8b0aaeb05802fc8
gasanadj <d.gasana@alustudent.com>
2023-06-09 17:32:55 +0200

    add responsiveness

--- script.js
+++ script.js
@@ -8,5 +8,70 @@ document.addEventListener("DOMContentLoaded", function () {
     slides[currentSlide].style.display = "block";
   }
 
-  setInterval(changeSlides, 2000);
+  setInterval(changeSlides, 5000);
 });
+
+// function changeBg() {
+//   var scrollValue = window.scrollY;
+//   const nav = document.getElementById('logo');
+//   const links = document.querySelectorAll('a');
+//   if (scrollValue > 50) {
+//     nav.style.background = '#fff';
+//     nav.style.boxShadow = '0 5px 5px -3px rgba(0,0,0,.15)'
+//     nav.style.color = '#2b8e8e'
+//     for (let i = 0; i<links.length; i++) {
+//       links[i].style.color = '#2b8e8e'
+//     }
+//   }
+//   else {
+//     nav.style.background = '#000001';
+//     nav.style.background =  'linear-gradient(0deg, transparent, rgba(0, 0, 0, 0.5))';
+//     nav.style.boxShadow = 'none'
+//     nav.style.color = '#fff'
+//     for (let i = 0; i<links.length; i++) {
+//       links[i].style.color = '#fff'
+//     }
+//   }
+// }
+
+//window.addEventListener('scroll', changeBg)
+
+const mainMenu = document.querySelector('nav > ul');
+const open = document.querySelector('.open');
+const closeMenu = document.querySelector('.close');
+open.addEventListener('click', show);
+closeMenu.addEventListener('click', hide);
+window.addEventListener('resize', handleResize);
+
+function show() {
+  mainMenu.style.display = 'flex';
+  setTimeout(function () {
+    mainMenu.style.top = '0';
+  }, 10);
+  open.style.display = 'none';
+  closeMenu.style.display = 'block';
+}
+
+function hide() {
+  mainMenu.style.top = '-100%';
+  setTimeout(function () {
+    mainMenu.style.display = 'none';
+  }, 1000);
+  closeMenu.style.display = 'none';
+  open.style.display = 'block';
+}
+
+function handleResize() {
+  if (window.innerWidth > 800) {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    open.style.display = 'none';
+    closeMenu.style.display = 'none';
+  } else {
+    mainMenu.style.display = 'none';
+    mainMenu.style.top = '-100%';
+    open.style.display = 'block';
+    closeMenu.style.display = 'none';
+  }
+}
+
