c9e2be6ceed0eb676c616d29811926badb8ddec7
gasanadj <d.gasana@alustudent.com>
2023-06-10 16:26:30 +0200

    add stats section

--- script.js
+++ script.js
@@ -77,3 +77,27 @@ function handleResize() {
   }
 }
 
+
+let section = document.querySelector('.numbers');
+let stat = document.querySelectorAll('.stats .sta');
+let start = false;
+
+window.onscroll = function() {
+  if (window.scrollY >= section.offsetTop) {
+    if(!start) {
+      stat.forEach((sta) => startCount(sta))
+    }
+    start = true
+  }
+}
+function startCount(el) {
+  let max = el.dataset.max;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == max) {
+      clearInterval(count);
+    }
+  }, 2000 / stat);
+}
+
+
