606432ac2e2db92729e2360e7d1639175750830e
gasanadj <d.gasana@alustudent.com>
2023-06-12 00:15:45 +0200

    add top button to scroll to top

--- script.js
+++ script.js
@@ -98,3 +98,20 @@ document.addEventListener('scroll', function() {
     }
   })
 })
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = 'block';
+  }else {
+    document.getElementById("scrollToTopBtn").style.display = 'none';
+  }
+}
